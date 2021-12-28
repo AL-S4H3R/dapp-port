0fda9c90e305e5e60aa1406bb58a84fb01071e02
Shlok D. Mange <shlok_mange@protonmail.com>
2021-12-28 20:33:14 +0400

    Get bytecode & ABI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,12 @@ import type { AppProps } from 'next/app'
 import { Fragment } from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
 import { MoralisProvider } from 'react-moralis'
-/*
-import { DAppProvider, Config, Polygon, Mumbai } from '@usedapp/core'
+
+import { DAppProvider, Config, Polygon, Mumbai, Mainnet } from '@usedapp/core'
 const config : Config = {
-  networks: [Polygon, Mumbai]
+  networks: [Polygon, Mumbai, Mainnet]
 }
-*/
+
 
 const appId = 'sQH0FwekBbFus2Luk2x1kMKV5J50GSw2RRhOVdzh'
 const serverUrl = 'https://mlxliwgdres9.usemoralis.com:2053/server'
@@ -16,13 +16,13 @@ const serverUrl = 'https://mlxliwgdres9.usemoralis.com:2053/server'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Fragment>
-      {/* <DAppProvider config={config}> */}
-      <MoralisProvider appId={appId} serverUrl={serverUrl}>
+      <DAppProvider config={config}>
+      {/* <MoralisProvider appId={appId} serverUrl={serverUrl}> */}
         <ChakraProvider>
           <Component {...pageProps} />
         </ChakraProvider>
-      </MoralisProvider>
-      {/* </DAppProvider> */}
+      {/* </MoralisProvider> */}
+      </DAppProvider>
     </Fragment>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,19 @@
 import { Box, HStack, Stack, Heading, Button, Text, Select, Input } from '@chakra-ui/react'
-// import { useEthers } from '@usedapp/core'
+import { useEthers } from '@usedapp/core'
 import type { NextPage } from 'next'
 import { Fragment, useState } from 'react'
 import { useMoralis } from 'react-moralis'
 import axios from 'axios'
 
 const Home: NextPage = () => {	
-	// const { activateBrowserWallet, account, chainId } = useEthers()
+	const { activateBrowserWallet, account, chainId, library } = useEthers()
 	
-	const { authenticate, isAuthenticated, user, account, chainId } = useMoralis()
+	// const { authenticate, isAuthenticated, user, account, chainId } = useMoralis()
 	const [ contractAddress, setContractAddress ] = useState('')
 	const [ selectChain, setSelectChain ] = useState('')
-
+	const [ abi, setAbi ] = useState('')
+	const [ bytecode, setBytecode ] = useState('')
+	
 	const etherscanApi = `https://api.etherscan.com/api?module=contract&action=getsourcecode`
 	const apiKey = process.env.NEXT_PUBLIC_ETHERSCAN_API_KEY
 
@@ -22,6 +24,16 @@ const Home: NextPage = () => {
 			store bytecode in a state var.	
 		*/
 
+		console.log('Executing function call')
+		try {
+			if(library){
+				const bytecode = await library.getCode(contractAddress)
+				console.log(bytecode.toString())
+			}
+		}
+		catch(err){
+			console.log(err)
+		}
 		// Get contract ABI, detect smart contract params and store abi in a state var
 		const res = await axios.get(
 			`${etherscanApi}&address=${contractAddress}&apiKey=${apiKey}`
@@ -36,24 +48,18 @@ const Home: NextPage = () => {
 					<HStack px={{ base: 4, md: 6, lg: 8 }} py={{ base: 4, lg: 8 }} justify={'space-between'}>
 						<Heading fontFamily={'Share Tech Mono'} color={'blackAlpha.900'}>portoDapp</Heading>
 						{
-							!account ?
-							<Button onClick={() => authenticate()} border={'2px'} fontFamily={'monospace'}>
-								Login
-							</Button> :
-							<Text >
-								{account.substring(0,10)}...{account.substring(38)}
-							</Text>
+							account ? <Text>Connected</Text> : <Button onClick={() => activateBrowserWallet()}>Login</Button>
 						}
-						<Text>Chain Id: {parseInt(chainId!, 16)}</Text>
 					</HStack>
 					<Select placeholder='Select Chain'>
   						<option value='eth' onClick={(e) => setSelectChain('eth')}>Ethereum Mainnet</option>
   						<option value='rinkeby' onClick={(e) => setSelectChain('rinkeby')}>Rinkeby Testnet</option>
   						<option value='kovan' onClick={(e) => setSelectChain('kovan')}>Kovan Testnet</option>
+						<option value='polygon' onClick={(e) => setSelectChain('polygon')}>Polygon Testnet</option>
 					</Select>
 					<Input placeholder='Contract Address' value={contractAddress} onChange={(e) => setContractAddress(e.target.value)}/>
 					<Input placeholder='Add Constructor Params (comma separated)'/>
-					<Button onClick={getContractDetails}>Deploy</Button>
+					<Button onClick={getContractDetails}>Get Contract</Button>
 				</Stack>
 			</Box>
 		</Fragment>
