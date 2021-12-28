80f43945e538a24c4479e01cc3503c2ad21b1da3
Shlok D. Mange <shlok_mange@protonmail.com>
2021-12-28 18:17:38 +0400

    Chakra ui setup & etherscan api

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
-import '../styles/globals.css'
+import '@fontsource/share-tech-mono'
 import type { AppProps } from 'next/app'
+import { Fragment } from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+import { MoralisProvider } from 'react-moralis'
+/*
+import { DAppProvider, Config, Polygon, Mumbai } from '@usedapp/core'
+const config : Config = {
+  networks: [Polygon, Mumbai]
+}
+*/
+
+const appId = 'sQH0FwekBbFus2Luk2x1kMKV5J50GSw2RRhOVdzh'
+const serverUrl = 'https://mlxliwgdres9.usemoralis.com:2053/server'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      {/* <DAppProvider config={config}> */}
+      <MoralisProvider appId={appId} serverUrl={serverUrl}>
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </MoralisProvider>
+      {/* </DAppProvider> */}
+    </Fragment>
+  )
 }
 
 export default MyApp
