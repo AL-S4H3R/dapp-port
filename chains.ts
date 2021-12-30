15796a1c98e4cc67a04c1a1b59aa2695059ea55f
Shlok D. Mange <shlok_mange@protonmail.com>
2021-12-30 08:42:14 +0400

    Added chain

---
+++ chains.ts
@@ -0,0 +1,18 @@
+export const chains = [
+    {
+        chainId: '0x89',
+        chain: 'Polygon Mainnet'
+    },
+    {
+        chainId: '0x13881',
+        chain: 'Polygon Testnet (Mumbai)'
+    },
+    {
+        chainId: '0x61',
+        chain: 'BSC Testnet'
+    },
+    {
+        chainId: '0x38',
+        chain: 'Binance Mainnet'
+    },
+]
